e0b31a3f8a904e65c4e45c266de9bf524288eb95
Paul Hunt <paul.zero@hotmail.com>
2020-05-15 00:08:09 +0100

    Some styling tweaks.

--- Triceratops.Dashboard/Views/HtmlHelperExtensions.cs
+++ Triceratops.Dashboard/Views/HtmlHelperExtensions.cs
@@ -9,14 +9,17 @@ namespace Triceratops.Dashboard.Views
 {
     public static class HtmlHelperExtensions
     {
-        public static IHtmlContent ConditionalRouteLink(this IHtmlHelper helper, bool showLink, string label, string routeName, object routeValues = null)
+        public static IHtmlContent ConditionalRouteLink(this IHtmlHelper helper, bool showLink, string label, string routeName, string elementClass, object routeValues = null)
         {
             if (!showLink)
             {
-                return new HtmlString($"<span class=\"disabled-link\">{label}</span>");
+                return new HtmlString($"<span class=\"button disabled-button\">{label}</span>");
             }
 
-            return helper.RouteLink(label, routeName, routeValues);
+            return helper.RouteLink(label, routeName, routeValues, new Dictionary<string, string>
+            {
+                ["class"] = elementClass
+            });
         }
     }
 }
