a724dfb606febe002e310480be3b2d544b9a0204
Paul Hunt <paul.zero@hotmail.com>
2020-05-20 02:00:14 +0100

    Some more Linux nonsense, maybe.

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+rm .env
 rm docker-compose.override.yml
+cp linux.env .env
 cp linux_docker-compose.override.yml docker-compose.override.yml
 docker build -t triceratops_volumemanager:latest -f VolumeManager.Dockerfile .
 docker-compose -f docker-compose.yml up -d
